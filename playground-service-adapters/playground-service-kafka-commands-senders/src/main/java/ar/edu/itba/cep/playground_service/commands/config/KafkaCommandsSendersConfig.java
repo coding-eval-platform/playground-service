a1bc5d53101bab84259243fa8f17eab6a4803c4c
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-11 01:12:36 -0300

    Add Kafka configuration stuff
    - Inclde some bean definitions and some kafka properties

--- playground-service-adapters/playground-service-kafka-commands-senders/src/main/java/ar/edu/itba/cep/playground_service/commands/config/KafkaCommandsSendersConfig.java
+++ playground-service-adapters/playground-service-kafka-commands-senders/src/main/java/ar/edu/itba/cep/playground_service/commands/config/KafkaCommandsSendersConfig.java
@@ -1,7 +1,12 @@
 package ar.edu.itba.cep.playground_service.commands.config;
 
+import com.bellotapps.the_messenger.commons.Message;
+import com.bellotapps.the_messenger.producer.BiConsumerMessageProducer;
+import com.bellotapps.the_messenger.producer.MessageProducer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 /**
  * Configuration class for the Kafka Command senders module.
@@ -11,4 +16,16 @@ import org.springframework.context.annotation.Configuration;
         "ar.edu.itba.cep.playground_service.commands.*.config"
 })
 public class KafkaCommandsSendersConfig {
+
+    /**
+     * Creates a bean of {@link BiConsumerMessageProducer} that allows sending messages.
+     *
+     * @param kafkaTemplate The underlying {@link KafkaTemplate} used by the {@link MessageProducer}.
+     * @return A bean of {@link MessageProducer}.
+     */
+    @Bean
+    public MessageProducer messageProducer(final KafkaTemplate kafkaTemplate) {
+        @SuppressWarnings("unchecked") final var template = (KafkaTemplate<String, Message>) kafkaTemplate;
+        return new BiConsumerMessageProducer((message, channel) -> template.send(channel, message));
+    }
 }
--- playground-service-adapters/playground-service-kafka-commands-senders/src/main/java/ar/edu/itba/cep/playground_service/commands/executor_service/config/ExecutorServiceProxyKafkaCommandsSendersConfig.java
+++ playground-service-adapters/playground-service-kafka-commands-senders/src/main/java/ar/edu/itba/cep/playground_service/commands/executor_service/config/ExecutorServiceProxyKafkaCommandsSendersConfig.java
@@ -1,5 +1,16 @@
 package ar.edu.itba.cep.playground_service.commands.executor_service.config;
 
+import ar.edu.itba.cep.playground_service.commands.executor_service.ExecutionRequestDtoMessageBuilderFactory;
+import ar.edu.itba.cep.playground_service.commands.executor_service.dto.ExecutionRequestDto;
+import ar.edu.itba.cep.playground_service.commands.executor_service.dto.ExecutionResultDto;
+import com.bellotapps.the_messenger.commons.payload.PayloadDeserializer;
+import com.bellotapps.the_messenger.commons.payload.PayloadSerializer;
+import com.bellotapps.the_messenger.json.JacksonJsonPayloadDeserializer;
+import com.bellotapps.the_messenger.json.JacksonJsonPayloadSerializer;
+import com.bellotapps.the_messenger.producer.MessageBuilderFactory;
+import com.bellotapps.the_messenger.transport.json.jackson.JacksonMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,4 +22,39 @@ import org.springframework.context.annotation.Configuration;
         "ar.edu.itba.cep.playground_service.commands.executor_service"
 })
 public class ExecutorServiceProxyKafkaCommandsSendersConfig {
+
+    /**
+     * Creates a bean of {@link JacksonJsonPayloadSerializer} of {@link ExecutionRequestDto}.
+     *
+     * @return A bean of {@link PayloadSerializer} of {@link ExecutionResultDto}.
+     */
+    @Bean
+    public PayloadSerializer<ExecutionRequestDto> executionRequestDtoPayloadSerializer() {
+        return new JacksonJsonPayloadSerializer<>(new ObjectMapper(), ExecutionRequestDto.class);
+    }
+
+    /**
+     * Creates a bean of {@link JacksonJsonPayloadDeserializer} of {@link ExecutionResultDto}.
+     *
+     * @return A bean of {@link PayloadDeserializer} of {@link ExecutionResultDto}.
+     */
+    @Bean
+    public PayloadDeserializer<ExecutionResultDto> executionResultDtoPayloadDeserializer() {
+        return new JacksonJsonPayloadDeserializer<>(new ObjectMapper(), ExecutionResultDto.class);
+    }
+
+    /**
+     * Creates a bean of {@link ExecutionRequestDtoMessageBuilderFactory}.
+     *
+     * @param executionRequestDtoPayloadSerializer A {@link PayloadSerializer} of {@link ExecutionRequestDto}.
+     * @return A bean of {@link MessageBuilderFactory} of {@link ExecutionRequestDto}.
+     */
+    @Bean
+    public MessageBuilderFactory<ExecutionRequestDto> executionRequestMessageBuilderFactory(
+            final PayloadSerializer<ExecutionRequestDto> executionRequestDtoPayloadSerializer) {
+        return new ExecutionRequestDtoMessageBuilderFactory(
+                "PlaygroundService",
+                executionRequestDtoPayloadSerializer,
+                JacksonMessage::new);
+    }
 }
