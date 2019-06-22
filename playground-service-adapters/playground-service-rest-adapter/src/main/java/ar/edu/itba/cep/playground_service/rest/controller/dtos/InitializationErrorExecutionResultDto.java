b7e55c80d708defe3d0c6c27000dc1352c6df873
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-22 05:51:55 -0300

    Add REST DTOs for the new results

---
+++ playground-service-adapters/playground-service-rest-adapter/src/main/java/ar/edu/itba/cep/playground_service/rest/controller/dtos/InitializationErrorExecutionResultDto.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.cep.playground_service.rest.controller.dtos;
+
+import ar.edu.itba.cep.playground_service.models.InitializationErrorExecutionResult;
+
+/**
+ * A Data Transfer Object that wraps a {@link InitializationErrorExecutionResult}.
+ */
+public final class InitializationErrorExecutionResultDto implements ExecutionResultDto {
+}
--- playground-service-adapters/playground-service-rest-adapter/src/main/java/ar/edu/itba/cep/playground_service/rest/controller/dtos/TimedOutExecutionResultDto.java
+++ playground-service-adapters/playground-service-rest-adapter/src/main/java/ar/edu/itba/cep/playground_service/rest/controller/dtos/TimedOutExecutionResultDto.java
@@ -1,8 +1,9 @@
 package ar.edu.itba.cep.playground_service.rest.controller.dtos;
 
+import ar.edu.itba.cep.playground_service.models.TimedOutExecutionResult;
+
 /**
- * A Data Transfer Object that indicates that an {@link ar.edu.itba.cep.playground_service.models.ExecutionResult}
- * failed because it timed out.
+ * A Data Transfer Object that wraps a {@link TimedOutExecutionResult}.
  */
 public final class TimedOutExecutionResultDto implements ExecutionResultDto {
 }
