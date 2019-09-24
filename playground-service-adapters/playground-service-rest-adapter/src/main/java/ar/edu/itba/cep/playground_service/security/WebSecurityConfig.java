64e3cc4ad8bdbf244ba914021e431032dd3f471c
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-09-24 01:45:48 -0300

    Add the authentication filter
    - Closes #22

---
+++ playground-service-adapters/playground-service-rest-adapter/src/main/java/ar/edu/itba/cep/playground_service/security/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.cep.playground_service.security;
+
+
+import ar.edu.itba.cep.security.EnableJwtAuthentication;
+import ar.edu.itba.cep.security.EnableRestSecurity;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for security aspects at the web layer.
+ */
+@Configuration
+@EnableRestSecurity
+@EnableJwtAuthentication
+public class WebSecurityConfig {
+}
